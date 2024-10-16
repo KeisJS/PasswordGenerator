3ef37c64c089a24f63722137757bf1e5a4a8fa46
vitaly <workicio@mail.ru>
2024-10-16 16:57:21 +0300

    T4: add layout password generator component

--- src/PasswordGenerator/index.tsx
+++ src/PasswordGenerator/index.tsx
@@ -1,10 +1,46 @@
 import { memo } from 'react'
+import styles from './index.module.css'
 
 const PasswordGenerator = () => {
   return (
-    <>
-      Password generator component
-    </>
+    <div className={ styles.mainContainer }>
+      <div className={ styles.resultPassword }>JdfdsJ&2</div>
+      <div className={ styles.buttons }>
+        <div className={ styles.buttons__button }>
+          <button type="button">
+            Создать пароль
+          </button>
+        </div>
+        <div className={ styles.buttons__button }>
+          <button type="button" disabled={true}>
+            Скопировать пароль
+          </button>
+        </div>
+      </div>
+      <div className={ styles.filters }>
+        <div className={ styles.filters__filter }>
+          <label>
+            <input type="checkbox" checked={ true } name="1"/>
+            <span>Строчные буквы</span>
+            <span>abvffdssskldkfjsdlfjdslfjsdlkfjsdslgjlsdrgjk</span>
+          </label>
+        </div>
+        <div className={ styles.filters__filter }>
+          <label>
+            <input type="checkbox" checked={ false } name="2"/>
+            <span>Заглавные буквы</span>
+            <span>abvffdssskldkfjsdlfjdslfjsdlkfjsdslgjlsdrgjk</span>
+          </label>
+        </div>
+        <div className={ styles.filters__filter }>
+          <label>
+            <input type="checkbox" checked={ false } name="3"/>
+            <span>Цифры</span>
+            <span>0123456789</span>
+          </label>
+        </div>
+      </div>
+    </div>
   )
 }
 
